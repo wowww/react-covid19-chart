4298d15b552e71cd2886b15dea0bc7d03a65a975
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-15 14:21:31 +0900

    feat. axios로 데이터 가져옴

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,7 +1,18 @@
 import { useState, useEffect } from 'react'
-
+import axios from 'axios'
 
 const Contents = () => {
+
+  useEffect (() => {
+    const fetchEvents = async() => {
+      const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+      console.log(res)
+
+    }
+
+    fetchEvents()
+  })
+
   return (
     <section>
       <h2>국내 코로나 현황</h2>
