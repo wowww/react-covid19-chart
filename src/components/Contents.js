45bd6d7312bfefef8b2bcc0973007a8998deb911
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-15 14:30:21 +0900

    docs. Bar의 confirmedData 추가

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
+import { Bar, Doughnut, Line } from 'react-chartjs-2'
 import axios from 'axios'
 
 const Contents = () => {
-
+  const [confirmedData, setConfirmedData] = useState({});
+  
   useEffect (() => {
     const fetchEvents = async() => {
       const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
@@ -17,7 +19,21 @@ const Contents = () => {
     <section>
       <h2>국내 코로나 현황</h2>
       <div className="contents">
-
+        <div>
+          <Bar 
+            data={confirmedData}
+            options={
+              ({
+                titile: {
+                  display: true,
+                  text: "누적 확진자 추이",
+                  fontsize: 16
+                }
+              },
+              { legend: { display: true, position: "bottom" } })
+            }
+          />
+        </div>
       </div>
     </section>
   )
