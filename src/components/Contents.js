ac6b6f54adde8a786263cdb0105d318fac50b1de
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-15 10:53:19 +0900

    docs. Contents 생성

---
+++ src/components/Contents.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+
+const Contents = () => {
+  return (
+    <section>
+      <h2>국내 코로나 현황</h2>
+      <div className="contents">
+
+      </div>
+    </section>
+  )
+}
+
+export default Contents
